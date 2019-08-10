0e177d870609c6ecb2874c838ed521afa68e7eda
guominS <52800170+guominS@users.noreply.github.com>
2019-08-09 21:23:45 -0400

    Add files via upload

---
+++ assets/data/guomininfo.ts
@@ -0,0 +1,8 @@
+import {GuoMin} from '../../app/guomin';
+export const GUOMININFO: GuoMin = {
+    sNumber: "991473414",
+    sName: "MingxiGuo",
+    sloginName: "guomin",
+    scampus:"Oakville",
+    assignmentTitle:"Assignment4"
+}
